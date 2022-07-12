3c1c944f0a7c04627ef24d13b70966a16781ff44
Micah Shallom <micahshallom@gmail.com>
2022-07-13 00:31:41 +0100

    added frontend

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask.logging import create_logger
-import logging
+from time import sleep
+from numpy import round
+import logging 
 
 import pandas as pd
 from sklearn.externals import joblib
@@ -18,14 +20,24 @@ def scale(payload):
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
 
-@app.route("/")
+# @app.route("/")
+# def home():
+#     html = f"<h3>Sklearn Prediction Home</h3>"
+#     return html.format(format)
+
+@app.route('/')
+@app.route('/home' , methods=['POST'])
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
+    return render_template('form.html' , title="Home")
+
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    """Performs an sklearn prediction
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/home' to submit form"
+
+    if request.method == 'POST':
+        """Performs an sklearn prediction
         
         input looks like:
         {
@@ -52,19 +64,50 @@ def predict():
         { "prediction": [ <val> ] }
         
         """
+
+        form_data = request.form
+        data = []
+        for key, value in dict(form_data).items():
+            data.append(( key, { "0": float(value) } ))
+
+        prep_data = dict(data)
+
+        # # Logging the input payload
+        # json_payload = request.json
+        # LOG.info(f"JSON payload: \n{json_payload}")
+        # inference_payload = pd.DataFrame(json_payload)
+
+        inference_payload = pd.DataFrame(prep_data)
+        LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+        # scale the input
+        scaled_payload = scale(inference_payload)
+        # get an output prediction from the pretrained model, clf
+        prediction = list(clf.predict(scaled_payload))
+        # TO DO:  Log the output prediction value
+        LOG.info(f"Output Prediction: {prediction}")
+        preds = jsonify({'prediction': prediction})
+        sleep(2)
+
+        return render_template('prediction.html', form_data = form_data, prediction=round(prediction[0], decimals=3))
     
-    # Logging the input payload
-    json_payload = request.json
-    LOG.info(f"JSON payload: \n{json_payload}")
-    inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
-    # scale the input
-    scaled_payload = scale(inference_payload)
-    # get an output prediction from the pretrained model, clf
-    prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
-    LOG.info(f"Output Prediction: {prediction}")
-    return jsonify({'prediction': prediction})
+
+
+    
+
+
+
+# @app.route("/predict" , methods=['GET','POST'])
+# def data():
+#     if request.method == 'GET':
+#         return f"The URL /data is accessed directly. Try going to '/home' to submit form"
+#     if request.method == 'POST':
+#         form_data = request.form
+#         data = []
+#         for key, value in dict(form_data).items():
+#             data.append((key, {"0": value}))
+#         prep_data = dict(data)
+#         return render_template('prediction.html', form_data = form_data)
+    
 
 if __name__ == "__main__":
     # load pretrained model as clf
--- frontend/main.py
+++ frontend/main.py
@@ -1,21 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request , session
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('form.html')
+    if request.method == 'GET':
+        # return f"The URL /data is accessed directly. Try going to '/home' to submit form"
+        return render_template('form.html' , title="Home")
+    if request.method == 'POST':
+        form_data = request.form
+        data = []
+        for key, value in dict(form_data).items():
+            data.append((key, {"0": value}))
+
+        prep_data = dict(data)
+
+        session['prep_data'] = prep_data
+    return render_template('form.html' , title="Home")
 
 
 @app.route("/predict" , methods=['GET','POST'])
 def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/home' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        print(form_data)
-        return render_template('prediction.html',form_data = form_data)
+    form_data = session.get('prep_data')
+    print(form_data)
+    return render_template('prediction.html', form_data = form_data)
     
 if __name__ == "__main__":
     app.run(debug=True)
